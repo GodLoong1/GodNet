2cb9364172151c9c4c0c4664d50160c585876eab
GodLoong1 <1444824804@qq.com>
2025-06-02 22:25:28 +0800

    refactor: 重构日志宏定义，简化错误日志记录方式，支持可变参数重载

--- include/godnet/util/logger.hpp
+++ include/godnet/util/logger.hpp
@@ -6,35 +6,42 @@
 #include "godnet/util/noncopyable.hpp"
 #include "godnet/util/system.hpp"
 
-#define GODNET_LOG(lv, msg, ...) \
-    if (godnet::Logger::Instance().getLevel() <= lv) \
-        godnet::Logger::Instance().getLog()->log( \
-            spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, \
-            lv, msg, ##__VA_ARGS__)
+#define GODNET_LOG(lv, ...)\
+    if (godnet::Logger::Instance().getLevel() <= spdlog::level::lv)     \
+        godnet::Logger::Instance().getLog()->log(                       \
+            spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION},    \
+            spdlog::level::lv,                                          \
+            __VA_ARGS__)
 
-#define GODNET_LOG_TRACE(...) \
-    GODNET_LOG(spdlog::level::trace, __VA_ARGS__)
+#define GODNET_LOG_TRACE(...)       GODNET_LOG(trace, __VA_ARGS__)
+#define GODNET_LOG_DEBUG(...)       GODNET_LOG(debug, __VA_ARGS__)
+#define GODNET_LOG_INFO(...)        GODNET_LOG(info, __VA_ARGS__)
+#define GODNET_LOG_WARN(...)        GODNET_LOG(warn, __VA_ARGS__)
+#define GODNET_LOG_ERROR(...)       GODNET_LOG(err, __VA_ARGS__)
+#define GODNET_LOG_CRITICAL(...)    GODNET_LOG(critical, __VA_ARGS__)
 
-#define GODNET_LOG_DEBUG(...) \
-    GODNET_LOG(spdlog::level::debug, __VA_ARGS__)
+#define GODNET_LOG_SYSERR_1(msg) \
+    GODNET_LOG(err, msg ", errno: {}", godnet::system::getErrnoMsg())
 
-#define GODNET_LOG_INFO(...) \
-    GODNET_LOG(spdlog::level::info, __VA_ARGS__)
+#define GODNET_LOG_SYSERR_N(msg, ...) \
+    GODNET_LOG(err, msg ", errno: {}", __VA_ARGS__, godnet::system::getErrnoMsg())
 
-#define GODNET_LOG_WARN(...) \
-    GODNET_LOG(spdlog::level::warn, __VA_ARGS__)
+#define GODNET_LOG_SYSERR_OVERLOAD( \
+    _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, NAME, ...) NAME
 
-#define GODNET_LOG_ERROR(...) \
-    GODNET_LOG(spdlog::level::err, __VA_ARGS__)
-
-#define GODNET_LOG_SYSERR(msg, ...) \
-    GODNET_LOG(spdlog::level::err, \
-               msg ", errno: {}", \
-               ##__VA_ARGS__, \
-               godnet::system::getErrnoMsg())
-
-#define GODNET_LOG_CRITICAL(...) \
-    GODNET_LOG(spdlog::level::critical, __VA_ARGS__)
+#define GODNET_LOG_SYSERR(...) \
+    GODNET_LOG_SYSERR_OVERLOAD(__VA_ARGS__, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_N, \
+        GODNET_LOG_SYSERR_1) (__VA_ARGS__)
 
 namespace godnet
 {
